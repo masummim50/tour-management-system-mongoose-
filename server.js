bdec481e7c046a88b0c068a02038fef68a308b59
unknown <masummim50@gmail.com>
2022-10-06 23:55:11 +0600

    vercel

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const  mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const app = require("./app");
 
-mongoose.connect(process.env.DATABASE_LOCAL).then(()=> {
+mongoose.connect(process.env.DATABASE_ONLINE).then(()=> {
     console.log("yay, database is connected")
 })
 
