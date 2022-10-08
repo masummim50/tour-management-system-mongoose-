0e47a41fe43284cdc3525ee3e7ac5967cfada354
unknown <masummim50@gmail.com>
2022-10-08 11:59:16 +0600

    f

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res)=> {
-    console.log("server running smoothly");
-    res.sendFile(__dirname + "./view/index.html")
+    console.log("server running, without __dirname");
+    res.sendFile("./view/index.html")
     
 })
 
